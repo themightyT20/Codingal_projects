5c05ed8b30a51fc965e2f20f2761e831c9cd57aa
themightyT20 <themightyt20@gmail.com>
2026-06-20 21:13:16 +0500

    new commit

---
+++ virus-detector.py
@@ -0,0 +1,10 @@
+from tkinter import*
+from tkinter import messagebox
+
+root=Tk()
+root.geometry("200x200")
+def msg():
+  messagebox.showwarning("Alert", "Stop virus found")
+button=Button(root,text="Scan for virus",command=msg)
+button.place(x=40,y=80)
+root.mainloop()
